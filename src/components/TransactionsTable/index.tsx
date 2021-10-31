02491b8c888d026a01efb6cba468936de765fa84
jonatas-melo-silva <jonatas891024@gmail.com>
2021-10-31 01:46:19 -0300

    :tada: Adicionado componente TransactionsTable e styles

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export const TransactionsTable: React.FC = () => {
+  return (<Container>
+    <table>
+      <thead>
+        <tr>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Gás de cozinha</td>
+          <td className="deposit">+ R$19.90</td>
+          <td>Lazer</td>
+          <td>10/04/2020</td>
+        </tr>
+        <tr>
+          <td>Safado</td>
+          <td className="withdraw">- R$30.00</td>
+          <td>Saúde</td>
+          <td>10/04/2020</td>
+        </tr>
+        <tr>
+          <td>Gás de cozinha</td>
+          <td className="deposit">+ R$19.90</td>
+          <td>Lazer</td>
+          <td>10/04/2020</td>
+        </tr>
+        <tr>
+          <td>Safado</td>
+          <td className="withdraw">- R$30.00</td>
+          <td>Saúde</td>
+          <td>10/04/2020</td>
+        </tr>
+      </tbody>
+    </table>
+  </Container>)
+}
