b98181caa2d415ec2c62cc8416d0d598bfd75328
jonatas-melo-silva <jonatas891024@gmail.com>
2021-11-01 11:07:10 -0300

    :tada: Adicionada rota para criar transação

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App'
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
   routes() {
     this.namespace = 'api'
-    this.get('/transactions', () => [
-      {
-        id: 1,
-        title: 'Transaction 1',
-        amount: 100,
-        category: 'Food',
-        createdAt: new Date()
-      },
-      {
-        id: 2,
-        title: 'Transaction 2',
-        amount: 200,
-        category: 'Food',
-        createdAt: new Date()
-      },
-      {
-        id: 3,
-        title: 'Transaction 3',
-        amount: 300,
-        category: 'Food',
-        createdAt: new Date()
-      },
-      {
-        id: 4,
-        title: 'Transaction 4',
-        amount: 400,
-        category: 'Food',
-        createdAt: new Date()
-      },
-      {
-        id: 5,
-        title: 'Transaction 5',
-        amount: 500,
-        category: 'Food',
-        createdAt: new Date()
-      }
-    ])
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
   }
 })
 
