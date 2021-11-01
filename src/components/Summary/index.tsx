ddd84d5c2f4cdfc07d009bddf9beeb1b45b8cb6b
jonatas-melo-silva <jonatas891024@gmail.com>
2021-11-01 00:32:16 -0300

    :tada: Atualizando títulos do summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -13,21 +13,21 @@ export const Summary: React.FC = () => {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>+ R$1000,00</strong>
+        <strong>+ R$ 1000,00</strong>
       </div>
       <div>
       <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>- R$ 500,00</strong>
       </div>
       <div className="highlight-background">
       <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>= R$ 500,00</strong>
       </div>
     </Container>
   )
